1bfa8dc6a9262595a4bcd25ddeaae4c29425309b
Diya Zhao <zdy.gogo@msn.com>
2016-07-08 13:28:00 -0700

    refactor,show fizzbuzz on screen,line the results on top

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,34 @@
-// count through number 1 to 100, figure out each one if it is divisble by 3, 5 or both;
-for (var i = 1; i<= 100; i++) {
-	//for each number, if it is divisble by 3;and see if it is also divisble by 5, if so
-	// print Fizzbuzz
-	//if it's only divisble by 3, print Fizz;
-	if (i%3 === 0) {
-		if(i%5 === 0){
-			console.log("Fizzbuzz");
-		}
 
-		else {
-			console.log('Fizz');
+
+
+ function checkNum(newNumber) {
+	var newNumber = prompt("Please enter a number,from 1 to 100")
+	var number = parseInt(newNumber)
+	for(var i = 1; i < number; i++) {
+
+		if (i%3 === 0) {
+			if(i%5 === 0){
+				$('.output').append("Fizzbuzz<br>");
+			}
+			else {
+				$('.output').append('Fizz<br>');
+			}
 		}
+		// if it divisble by 5, print Buzz;
+		else if(i%5 === 0) {
+			$('.output').append("Buzz<br>");
+		}
+		// if it is not divisble by 3 and 5, print number itself;
+		else{
+			$('.output').append(i+"<br>");
+		}	
 	}
-	// if it divisble by 5, print Buzz;
-	else if(i%5 === 0) {
-		console.log("Buzz");
-	}
-	// if it is not divisble by 3 and 5, print number itself;
-	else{
-		console.log(i);
-	}
-};
+	
+}
+checkNum();
+
+
+
+	
+
+
